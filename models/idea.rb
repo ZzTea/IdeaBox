f29f27a95d94b4d38511d0af251d6a9c6ee7da65
ZzTea <billsong6919@gmail.com>
2017-11-14 16:27:32 +0800

    Add edit and revise logic of code.

--- models/idea.rb
+++ models/idea.rb
@@ -1,32 +1,9 @@
-require 'yaml/store'
-
 class Idea
-  def self.all
-    raw_ideas.map do |data|
-      new(data[:title], data[:description])
-    end
-  end
-
-  def self.delete(position)
-    database.transaction do |db|
-      database['ideas'].delete_at(position)
-    end
-  end
-
-  def self.raw_ideas
-    database.transaction do |db|
-      db['ideas'] || []
-    end
-  end
- 
-  def self.database
-    @database ||= YAML::Store.new('ideabox')
-  end
-  
   attr_accessor :title, :description
-  def initialize(title, description)
-    @title = title
-    @description = description
+
+  def initialize(attributes)
+    @title = attributes[:title]
+    @description = attributes[:description]
   end
 
   def save
@@ -37,6 +14,6 @@ class Idea
   end
 
   def database
-    @database ||= YAML::Store.new "ideabox"
+    @database ||= YAML::Store.new('ideabox')    
   end
 end
